28dae451f72461a9250faf6fa84955eee8014ea9
hotszcheung <jackho2010@hotmail.com>
2017-03-10 20:36:33 +0800

    update
    mysql.py 2017 3 10 20:36

--- mysql.py
+++ mysql.py
@@ -14,7 +14,7 @@ import Adafruit_DHT
 #username = root
 #password = 287730849
 #db = test1
-conn = MySQLdb.connect("localhost","root" ,"" ,"test1")
+conn = MySQLdb.connect("localhost","root" ,"287730849" ,"test1")
 
 
 try:
@@ -25,20 +25,24 @@ try:
                 print datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
                 print 'Temperature: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
-
-
+                li = str(readLight())
+                print 'The Light_intensity: '+ str(readLight())+ ' lux'
 
                 # MYSQL DATA Processing
                 c = conn.cursor()
 
-                c.execute("INSERT INTO sensor_data (datetime, temperature, humidity) VALUES (%s ,%s ,%s)" ,
-                (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), temperature, humidity))
+                #c.execute("INSERT INTO sensor_data (datetime, temperature, humidity, light_intensity) VALUES ($s ,$s ,$s ,$s)" ,
+                #[( datetime.now().strftime('%Y-%m-%d %H:%M:%S') ,temperature ,humidity ,li )]  )
+                c.execute("INSERT INTO sensor_data (datetime, temperature, humidity, light_intensity, air_pressure) VALUES (%s ,%s ,%s, %s, %s)" ,
+                ( datetime.now().strftime('%Y-%m-%d %H:%M:%S'), temperature, humidity, li, 10)   )
+
 
                 conn.commit()
 
                 print "DB insert data success."
                 print
                 time.sleep(58)
+
 
 except KeyboardInterrupt:
 
